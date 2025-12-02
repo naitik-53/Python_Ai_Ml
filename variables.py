9b2f095795e5b55a5b7ed5a5ccc7d2c937e3b8ce
Naitik Kumar <naitikkumar6654@gmail.com>
2025-12-02 20:27:51 +0530

    day1

--- variables.py
+++ variables.py
@@ -6,6 +6,12 @@ name = input("Enter Your Name: ")
 print("Hello", name, "Welcome To Python Programming");
 age = int(input("Enter Your Age: "))
 print("Your Age Is:", age);
+school_name = input("Enter Your School Name: ")
+print("Your School Name Is;", school_name);
+bank = input("Enter Your Bank Name: ")
+print("Your Bank Name Is:", bank);
+
+
 
 
 #Program To Take Input From THe User
