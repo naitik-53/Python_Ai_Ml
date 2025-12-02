58a796a9a8c31926208b6b9aa83c1e33c7f45010
Naitik Kumar <naitikkumar6654@gmail.com>
2025-12-02 20:35:28 +0530

    day 1

--- assignment2.py
+++ assignment2.py
@@ -6,3 +6,19 @@ area = math.pi * radius * radius
 print("The Area Of Circle Is:", area)   
 
 
+#Take Radius As Input And Calculate Circumference Of Circle
+radius = float(input("Enter Radius Of Circle: "))
+circumference = 2 * math.pi * radius
+print("The Circumference Of Circle Is:", circumference)
+
+#Take Length And Breadth As Input And Calculate Area Of Rectangle
+length = float(input("Enter Length Of Rectangle: "))
+breadth = float(input("Enter Breadth Of Rectangle: "))
+area_rectangle = length * breadth
+print("The Area Of Rectangle Is:", area_rectangle)
+
+#Take Base And Height As Input And Calculate Area Of Triangle
+base = float(input("Enter Base Of Triangle: "))
+height = float(input("Enter Height Of Triangle: "))
+area_triangle = 0.5 * base * height
+print("The Area Of Triangle Is:", area_triangle)
--- firstprogram.py
+++ firstprogram.py
@@ -2,3 +2,8 @@ print("Hello I Am Naitik Kumar");
 print("I Am Learning Python Programming Language");
 print("I Am Enjoying It Very Much");
 print("I Hope To Become A Good Programmer Soon");
+print("Thank You");
+print("Welcome Naitik Kumar To Python Programming \n" * 10);
+print("Python Is Fun \n" * 5);
+print("I Love Python \n" * 7);
+print("Programming Is Awesome \n" * 8);
