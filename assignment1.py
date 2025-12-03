bbdf706f74851cdc66df260857719b2cbba26ffe
Naitik Kumar <naitikkumar6654@gmail.com>
2025-12-03 17:49:23 +0530

    day2

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,4 @@
 #Print Your Name 10 Times
-print("NaitikKumar\n" * 10)
+print("NaitikKumar\n" * 10);
+print("My Father's Name Is Murari Kumar Mandal\n" * 10);
+print("I want to learn ethical hacking\n" * 10);
