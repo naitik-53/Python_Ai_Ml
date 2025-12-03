199fea2aed439a0b789b511402f63e5ab5fa2e5d
Naitik Kumar <naitikkumar6654@gmail.com>
2025-12-03 22:22:47 +0530

    day2

---
+++ string_basic.py
@@ -0,0 +1,41 @@
+str1 = "Hello"
+str2 = 'World'
+str3 = '''This Is A STring '''
+print(str1 + str2)
+
+#string concatination
+str4 = (str1 + str2)
+print(len(str4));
+
+#Indexing
+str5 = "Python";
+print(str5[0]);
+print(str5[1]);
+print(str5[2]);
+print(str5[3]);
+print(str5[4]);
+print(str5[5]);
+
+#Slicing
+str6 = "Programming"
+print(str6[0:3])  # Pro
+
+print(str6[3:7])  # gram
+print(str6[7:11]) # ming
+
+print(str6[:6])   # Progra
+print(str6[6:])   # mming
+print(str6[:])    # Programming
+print(str6[-1])   # g
+print(str6[-4:-1]) # ming   
+
+
+#Immutable Mtalab Jo Badla na ja sake
+
+#Indexing - string ke ander her character ko aik position di jati hai
+str7 = "Immutable"
+# str7[0] = 'i'  # This will raise an error
+print(str7[0]);
+
+
+
